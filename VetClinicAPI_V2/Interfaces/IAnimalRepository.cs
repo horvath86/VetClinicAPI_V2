ae12286ab7007ca77d53ee7bda9b7ad98b3a4226
Vladimir Horvat <horvat.vladimir@uns.ac.rs>
2026-06-16 22:24:19 +0200

    Finished IAnimalRepository

---
+++ VetClinicAPI_V2/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,14 @@
+﻿using VetClinicAPI_V2.DTO.Requests;
+using VetClinicAPI_V2.DTO.Responses;
+
+namespace VetClinicAPI_V2.Interfaces
+{
+    public interface IAnimalInterface
+    {
+        public Task<ICollection<AnimalResponseDTO>> GetAllAnimals(string? search = null);
+        public Task<AnimalResponseDTO?> GetAnimalById(int id);
+        public Task<AnimalResponseDTO> CreateAnimal(AnimalCreateDTO createAnimal);
+        public Task<AnimalResponseDTO?> UpdateAnimal(AnimalUpdateDTO updateAnimal);
+        public Task<bool> DeleteAnimal(int id);
+    }
+}
