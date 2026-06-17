b16bf4b81438c136308e7f730695a58e4db903bf
Vladimir Horvat <horvat.vladimir@uns.ac.rs>
2026-06-17 13:08:26 +0200

    Created IAppointmentRepository

---
+++ VetClinicAPI_V2/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using VetClinicAPI_V2.DTO.Requests;
+using VetClinicAPI_V2.DTO.Responses;
+
+namespace VetClinicAPI_V2.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        Task<ICollection<AppointmentResponseDTO>> GetallAppointmentsAsync(string? search);
+        Task<AppointmentResponseDTO> GetAppointmentByIdAsync(int id);
+        Task<AppointmentResponseDTO> CreateAppointmentAsync(AppointmentCreateDTO createAppointment);
+        Task<AppointmentResponseDTO> UpdateAppointmentAsync(AppointmentUpdateDTO updateAppointment);
+        Task<bool> DeleteAppointmentAsync(int id);
+    }
+}
